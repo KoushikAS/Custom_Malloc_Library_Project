26484bd29ce820fe7c62f41499ac35fbf03ff216
Koushik Annareddy Sreenath <koushik.annareddysreenath@duke.edu>
2023-01-21 07:26:51 -0500

    Fixed lot of stupid bugs however assertion statemnts are still there should remove all of it

--- my_malloc/my_malloc.c
+++ my_malloc/my_malloc.c
@@ -1,6 +1,8 @@
 #include "my_malloc.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include<assert.h>
+#include<stdbool.h>
 
 mem_block_list *createNewMetaDataNode(void * mem, size_t size){
      mem_block_list *metadata = (mem_block_list *)mem;
@@ -10,28 +12,76 @@ mem_block_list *createNewMetaDataNode(void * mem, size_t size){
      return metadata;     
 }
 
+void addMetadataToList(mem_block_list **head,  mem_block_list **metadata){
+
+    if(*head == NULL) { //Adding first element in the list
+       *head = *metadata;
+       return;
+     }
+  
+     if(*head > *metadata){ //Adding to the front (i.e. Head)
+       (*metadata)->next = *head;
+       (*head)->prev = *metadata;
+       *head = *metadata;
+       return; 
+     }
+  
+     mem_block_list *curr = *head;
+     while(curr->next != NULL && curr->next < *metadata){
+       curr = curr->next;
+     }
+
+     (*metadata)->next = curr->next;
+     (*metadata)->prev = curr;
+     if(curr->next != NULL){ //To check if it is not the end of list.
+       curr->next->prev = *metadata;
+     }
+     curr->next = *metadata;
+}
+
 void *expandHeap(size_t size){
      void *allocated_mem = sbrk(sizeof(mem_block_list) + size);
-     createNewMetaDataNode(allocated_mem, size);    
+     mem_block_list *metadata = createNewMetaDataNode(allocated_mem, size);
+     addMetadataToList(&alloc_head, &metadata);
+
+     	 /**testing**/
+	mem_block_list *curr_test = free_head;
+	while(curr_test != NULL){
+	  	  assert(curr_test != metadata);
+	  curr_test = curr_test->next;
+	}
+     
      return (void *)allocated_mem + sizeof(mem_block_list);
 }
 
 void *allocateFromFreeSpace(mem_block_list *curr){
-	mem_block_list *prev = curr->prev;
-	mem_block_list *next = curr->next;
+	mem_block_list *prev_free = curr->prev;
+	mem_block_list *next_free = curr->next;
 
-	if(prev == NULL){ //1st element
-	  free_head = next;
+	//Make sure the Free list is proper
+	if(prev_free == NULL){ //1st element
+	  free_head = next_free;
 	} else {
-	  prev->next = next;
+	  prev_free->next = next_free;
 	}
 
-	if(next != NULL){
-	  next->prev = prev;
+	if(next_free != NULL){
+	  next_free->prev = prev_free;
 	}
 
+	//Allocating the new memory
+	
 	curr->next = NULL;
 	curr->prev = NULL;
+
+	addMetadataToList(&alloc_head, &curr);
+
+	 /**testing**/
+	mem_block_list *curr_test = free_head;
+	while(curr_test){
+	  assert(curr_test != curr);
+	  curr_test = curr_test->next;
+	}
 	  
 	return (void *)curr + sizeof(mem_block_list);  
 }
@@ -51,6 +101,9 @@ void *ff_malloc(size_t size){
 	  mem_block_list *metadata = createNewMetaDataNode((void *)curr + sizeof(mem_block_list) + size, extra_space - sizeof(mem_block_list));
 	  metadata->prev = curr;
 	  metadata->next = curr->next;
+	  if(curr->next != NULL){
+	    curr->next->prev = metadata;
+	  }
 	  curr->next = metadata;
 	  curr->len = size;
 	}
@@ -65,40 +118,17 @@ void *ff_malloc(size_t size){
     return expandHeap(size);
 }
 
-void addNewNode(void *ptr){
-  mem_block_list *metadata = (mem_block_list *)(ptr - sizeof(mem_block_list));
-  
-  if(free_head == NULL) { //Adding first element in the list
-    free_head = metadata;
-    return;
-  }
-  
-  if(free_head > metadata){ //Adding to the front (i.e. Head)
-    metadata->next = free_head;
-    free_head->prev = metadata;
-    free_head = metadata;
-    return; 
-  }
-  
-  mem_block_list *curr = free_head;
-  while(curr->next != NULL && curr->next < metadata){
-    curr = curr->next;
-  }
-
-  metadata->next = curr->next;
-  metadata->prev = curr;
-  if(curr->next != NULL){ //To check if it is not the end of list.
-    curr->next->prev = metadata;
-  }
-  curr->next = metadata;
-}
 
 void coalesce(){ 
   mem_block_list *curr = free_head;
   while(curr != NULL){
     if((curr->next != NULL) && ((void *)curr + curr->len + sizeof(mem_block_list) == curr->next)){
-	curr->len = curr->len + curr->next->len + sizeof(mem_block_list); //Also adding space of mem_block_list 
-	curr->next = curr->next->next;
+	curr->len = curr->len + curr->next->len + sizeof(mem_block_list); //Also adding space of mem_block_list
+	mem_block_list *next = curr->next->next;
+	if(next != NULL){
+	  next->prev = curr;
+	}
+	curr->next = next;
     } else{
       curr = curr->next;
     }
@@ -106,19 +136,40 @@ void coalesce(){
 }
 
 void ff_free(void * ptr){
-  addNewNode(ptr);
+  mem_block_list *metadata = (mem_block_list *)(ptr - sizeof(mem_block_list));
+
+  mem_block_list *prev_alloc = metadata->prev;
+  mem_block_list *next_alloc = metadata->next;
+
+  //Make sure the Free list is proper
+  if(prev_alloc == NULL){ //1st element
+    alloc_head = next_alloc;
+  } else {
+    prev_alloc->next = next_alloc;
+  }
+
+  if(next_alloc != NULL){
+    next_alloc->prev = prev_alloc;
+  }
+
+  metadata->prev = NULL;
+  metadata->next = NULL;
+  
+  addMetadataToList(&free_head, &metadata);
   coalesce();
+
+  /**testing**/
+  mem_block_list *curr = alloc_head;
+  while(curr){
+    assert(curr != metadata);
+    curr = curr->next;
+  }
 }
 
-unsigned long get_data_segment_size(){  
-  return 8L;
-}
-
-
-unsigned long get_data_segment_free_space_size(){
+unsigned long get_segment_size(mem_block_list *head){
   unsigned long size =0;
 
-  mem_block_list *curr = free_head;
+  mem_block_list *curr = head;
 
   while(curr != NULL){
     size = size + sizeof(mem_block_list) + curr->len;
@@ -126,4 +177,13 @@ unsigned long get_data_segment_free_space_size(){
   }
 
   return size;
+}
+
+unsigned long get_data_segment_size(){  
+  return get_segment_size(alloc_head) + get_segment_size(free_head);
+}
+
+
+unsigned long get_data_segment_free_space_size(){
+  return get_segment_size(free_head);
 }
